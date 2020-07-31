3962b9b52a6bf69352a409dccc8bf3d3c3c39a66
Scott H Lee <slee@logicfirstllc.com>
2020-07-31 00:10:31 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -14,6 +14,6 @@ def list_dwarves(array)
   # Print a numbered list of each element
   
   array.each_with_index { |element, i|
-    p (i + 1).to_s + element }
+    p (i + 1).to_s + ". " + element }
     
 end
