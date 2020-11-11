c87a5690ca856ebc925d109c309a57a073f565d2
logicfirst <slee@logicfirstllc.com>
2020-11-11 14:30:13 -0500

    commit tree 68605f5cfaed3a674df01b656cd7ff8ba7ed2aca

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -2,18 +2,16 @@ def greet_characters(array)
   # Use `each` to enumerate over the provided array
   #
   # Print a custom greeting for each element
-  
-  array.each { |element| 
-    p "Hello " + element + "!"}
-    
+  array.each do |element|
+    puts "Hello #{element}!"
+  end
 end
 
 def list_dwarves(array)
   # Use `each_with_index` to enumerate over the provided array
   #
   # Print a numbered list of each element
-  
-  array.each_with_index { |element, i|
-    p (i + 1).to_s + ". " + element }
-    
+  array.each_with_index do |element, i|
+    puts "#{(i + 1)}. #{element}"
+  end
 end
